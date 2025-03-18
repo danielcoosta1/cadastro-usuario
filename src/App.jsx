c7eef14e8c406217a52db0bf4112e011057f25b3
Daniel Costa <daniel.dc697@gmail.com>
2025-03-18 16:12:05 -0300

    Fazendo a base do html da pagina HOME(inicial)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import {
+  Main,
+  DivImagem,
+  Form,
+  Tittle,
+  DivInputs,
+  Input,
+  Label,
+  Button
+} from "./assets/styles.js";
 
 function Home() {
   return (
     <>
-    
+      <Main>
+        <DivImagem>
+          <img />
+        </DivImagem>
 
+        <Form>
+          <Tittle>Cadastro de Usuário</Tittle>
+          <DivInputs>
+            <div>
+              <Label for="nome"> Nome do usuário</Label>
+              <Input type="text" id="nome"></Input>
+              <Label for="idade"> Idade</Label>
+              <Input type="text" id="idade"></Input>
+            </div>
+            <Label for="email"> Digite seu e-mail</Label>
+            <Input type="email" id="email"></Input>
+          </DivInputs>
+          <Button>Cadastrar</Button>
+        </Form>
+      </Main>
     </>
   );
 }
---
+++ src/assets/styles.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+
+const Main = styled.main`
+
+`;
+
+const DivImagem = styled.div`
+`;
+
+const Form = styled.form`
+`;
+
+const Tittle = styled.h1`
+`;
+
+const DivInputs = styled.div`
+`;
+
+const Label = styled.label`
+`;
+
+const Input = styled.input`
+
+`;
+
+const Button = styled.button`
+
+`;
+
+
+
+
+
+
+export {
+    Main, DivImagem, Form,
+    Tittle, DivInputs,  Label, Input, 
+    Button,
+
+
+}
