21510ba8c96e98d1f4130e0e4e2a651ef055a940
Daniel Costa <daniel.dc697@gmail.com>
2025-05-19 16:52:30 -0300

    o ideal é só retornar um booleano de sucesso:

--- src/pages/listUsers/deletarUsuario.js
+++ src/pages/listUsers/deletarUsuario.js
@@ -1,10 +1,9 @@
 import api from "../../services/api";
 
-
 export async function deletarUsuario(id) {
   try {
-    const response = await api.delete(`/${id}`); // <-- Corrigido
-    return response.data;
+    await api.delete(`/${id}`);
+    return true; // Sucesso
   } catch (error) {
     console.error("Erro ao deletar usuário", error);
     throw error;
