10546d64b3372d3f4f2db5388643f8f816b85a3b
Daniel Costa <daniel.dc697@gmail.com>
2025-05-20 09:37:51 -0300

    Reponsividade para o Home

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,74 +1,83 @@
 import styled from "styled-components";
 
+// CONTAINER PRINCIPAL
 const Main = styled.main`
-
-
   height: 100vh;
   width: 100vw;
 
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 2rem 1rem; /* para dar respiro lateral em telas menores */
 `;
 
-//TOP CONTAINER
-
-const DivImagem = styled.div`
-  background: linear-gradient(to right, #fe7e5d, #7f3841);
-
-  margin: 5rem auto;
-
-  height: 30%;
-  width: 50%;
-  max-width: 75rem;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  border-radius: 30px;
-`;
-
-const Img = styled.img`
-  max-width: 100%;
-  max-height: 100%;
-`;
-
-// FORM
-
+// FORMULÁRIO
 const Form = styled.form`
-  margin: 0 auto;
+  margin: 2rem auto;
   max-width: 75rem;
-  height: 50%;
+  width: 100%;
+  height: auto;
 
   display: flex;
   flex-direction: column;
   gap: 2rem;
-
   align-items: center;
+  justify-content: center;
+
+  @media (max-width: 1200px) {
+    max-width: 60rem;
+  }
+
+  @media (max-width: 768px) {
+    gap: 1.5rem;
+    max-width: 100%;
+    margin: 0;
+  }
 `;
 
 const Tittle = styled.h1`
   color: #fff;
   font-size: 2.5rem;
   font-weight: 600;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    font-size: 1.6rem;
+  }
 `;
 
 const ListaInputs = styled.ul`
   display: flex;
   flex-direction: column;
+  
+
   gap: 1rem;
+  width: 100%;
+  @media (max-width: 768px) {
+    width: 80%;
+    margin-left: -25px;
+  }
 `;
 
 const DivLinha1 = styled.div`
   display: flex;
   gap: 2rem;
+  width: 100%;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 1rem;
+  }
 `;
-const DivLinha2 = styled.div``;
+
+const DivLinha2 = styled.div`
+  width: 100%;
+`;
 
 const ListaItem = styled.li`
   display: flex;
   flex-direction: column;
+  flex: 1;
 `;
 
 const Label = styled.label`
@@ -83,19 +92,42 @@ const Label = styled.label`
   }
 `;
 
-// const Span = styled.span`
-//   color: red;
-// `;
-
 const Input = styled.input`
   border-radius: 10px;
   border: 1px solid #d2dae2;
   padding: 0.5rem 1rem;
   background-color: #fff;
   outline: none;
+  width: 100%;
 `;
 
-// BUTTON -
+// CASO USE A DIV DE IMAGEM NO FUTURO
+const DivImagem = styled.div`
+  background: linear-gradient(to right, #fe7e5d, #7f3841);
+  margin: 5rem auto;
+  height: 30%;
+  width: 50%;
+  max-width: 75rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 30px;
+
+  @media (max-width: 1200px) {
+    width: 70%;
+  }
+
+  @media (max-width: 768px) {
+    width: 90%;
+    height: 20%;
+  }
+`;
+
+const Img = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+`;
 
 export {
   Main,
